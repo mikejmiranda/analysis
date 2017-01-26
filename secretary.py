897bf30d7848a0ecaf32942035fb6b33bda495e2
mikejmiranda <mikejmiranda@gmail.com>
2017-01-25 16:16:27 -0800

    Update secretary.py
    misunderstood the problem and calculated the wrong thing...

--- secretary.py
+++ secretary.py
@@ -7,26 +7,25 @@ trials = 1000
 saved_trials = pd.DataFrame()
 
 
-for n in range(trials):
-    df = pd.DataFrame(np.random.randint(1,100,size=(sample,1)), columns=list(['Rating']))
-    df['Candidate'] = df.index+1
-    df['Rank'] = df['Rating'].rank(ascending=False, method='min')
+for m in range(trials):
+    df = pd.DataFrame(np.arange(1,101), columns=['Rating'])
+    df = df.sample(frac=1).reset_index(drop=True)
     n = round(len(df)/math.e)
-    min_rating = df.loc[df['Candidate'] <= n, 'Rating'].max()
+    min_rating = df.loc[df.index <= n, 'Rating'].max()
     
     #need to find the first candidate after n with a rating > min_rating
     k = n+1
     while True:
-        if k == df['Candidate'].max():
-            saved_trials = saved_trials.append(df.loc[k-1])
+        if k == df.index.max():
+            saved_trials = saved_trials.append(df.loc[df.index.max()])
             break
-        elif df['Rating'][k-1] >= min_rating:
-            saved_trials = saved_trials.append(df.loc[k-1])
+        elif df['Rating'][k] > min_rating:
+            saved_trials = saved_trials.append(df.loc[k])
             break
-        elif df['Rating'][k-1] < min_rating:
+        elif df['Rating'][k] <= min_rating:
             k = k+1
 
 saved_trials = saved_trials.reset_index()
 
           
-len(saved_trials[saved_trials['Rank']==1])/len(saved_trials)
+len(saved_trials[saved_trials['Rating']==sample])/len(saved_trials)
